backprop fix

neural/layer.py
@@ -29,9 +29,9 @@ class Layer:
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.forward(X)[:, 0]
 
-    def update_weights(self, errors: np.ndarray) -> np.ndarray:
-        deltas = np.zeros((len(self.units), errors.size))
+    def update_weights(self, delta: np.ndarray) -> np.ndarray:
+        deltas = np.zeros(len(self.units))
         for i, u in enumerate(self.units):
-            deltas[i] = u.update_weights(errors)
+            deltas[i] = u.update_weights(delta)
 
-        return deltas[0, :]
+        return deltas

neural/neuron.py
@@ -38,4 +38,4 @@ class Neuron:
         self.W -= self.learning_rate * weights_gradient
         self.b -= self.learning_rate * bias_gradient
 
-        return delta
+        return np.sum(delta, keepdims=True)

test/network.py
@@ -6,7 +6,7 @@ from sklearn.neural_network import MLPClassifier
 from neural.network import Network
 
 if __name__ == "__main__":
-    n_features = 20
+    n_features = 2
     X, y = [
         np.array(i)
         for i in make_classification(

test/neuron.py
@@ -23,13 +23,12 @@ if __name__ == "__main__":
     neuron = Neuron(activation="logistic", learning_rate=0.1)
     neuron.init_weights(X.shape[1])
 
-    batch_size = 1
+    batch_size = 2
     for epoch in range(200):
         for i in range(0, len(y), batch_size):
             out = neuron(X[i : i + batch_size, :])
-            error = out - y[i : i + batch_size]
-            error = 2 * error / batch_size
-            delta = neuron.update_weights(error)
+            error = np.sum(out - y[i : i + batch_size])
+            delta = neuron.update_weights(2 * error)
 
     out = np.round(neuron(X))
     accuracy = accuracy_score(y, out)