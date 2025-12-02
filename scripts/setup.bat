cb787ab72f32e91aaf1b99a7638772f6c5fc21e8
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-02 01:37:30 +0100

    obs

--- scripts/setup.bat
+++
@@ -1,6 +0,0 @@
-
-python -m venv .env
-.env\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-pip install -e .
--- scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-python -m venv .env
-source .env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install -e .
