aef4da52d6d292d2b107a98da86ac9fe7a52d625
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-11 19:29:51 +0100

    multi output fit

--- neural/network.py
+++ neural/network.py
@@ -6,12 +6,12 @@ from neural.layer import Layer
 class Network:
     def __init__(
         self,
-        hidden_layer_sizes=(5,),
+        hidden_layer_sizes=(100,),
         activation: str = "logistic",
-        learning_rate: float = 0.1,
+        learning_rate: float = 0.01,
         lam: float = 0.0001,  # regularization
         alpha: float = 0.5,  # momentum
-        tol: float = 1e-4,
+        tol: float = 1e-5,
         batch_size: int = 10,
         shuffle: bool = False,
         max_iter: int = 200,
@@ -43,16 +43,23 @@ class Network:
             input_size = layer.units
 
     def forward(self, X: np.ndarray) -> np.ndarray:
+        out = X.copy()
         for layer in self.layers:
-            X = layer.forward(X)
+            out = layer.forward(out)
 
-        return X
+        return out
 
     def backward(self, dloss: np.ndarray) -> None:
         for layer in reversed(self.layers):
             dloss = layer.backward(dloss)
 
-    def fit(self, X, y):
+    def fit(
+        self,
+        X: np.ndarray,
+        y: np.ndarray,
+        X_val: None | np.ndarray = None,
+        y_val: None | np.ndarray = None,
+    ):
         # initialize weights
         self.init_weights(X.shape[1])
 
@@ -63,6 +70,8 @@ class Network:
         indices = np.arange(X.shape[0])
 
         self.loss_curve = []
+        self.val_loss_curve = []
+
         best_loss = np.inf
         stop_counter = 0
         for _ in range(self.max_iter):
@@ -70,16 +79,21 @@ class Network:
             if self.shuffle:
                 np.random.shuffle(indices)
 
-            for i in range(0, len(y), self.batch_size):
+            for i in range(0, y.shape[0], self.batch_size):
                 batch_idx = indices[i : i + self.batch_size]
                 out = self.forward(X[batch_idx])
-                error = out - y[batch_idx].reshape(-1, 1)
+                error = out - y[batch_idx]
 
                 # backpropagation
-                self.backward(2 * error)
+                self.backward(2 * error / error.shape[1])
 
-            out = self.forward(X)[:, 0]
+            # epoch loss
+            out = self.forward(X)
             self.loss_curve.append(np.mean((out - y) ** 2))
+
+            if X_val is not None:
+                out = self.forward(X_val)
+                self.val_loss_curve.append(np.mean((out - y_val) ** 2))
 
             # stopping criteria
             if abs(best_loss - self.loss_curve[-1]) < self.tol:
@@ -99,12 +113,12 @@ class Network:
 class Classifier(Network):
     def __init__(
         self,
-        hidden_layer_sizes=(5,),
+        hidden_layer_sizes=(100,),
         activation: str = "logistic",
-        learning_rate: float = 0.1,
+        learning_rate: float = 0.01,
         lam: float = 0.0001,
         alpha: float = 0.5,
-        tol: float = 1e-4,
+        tol: float = 1e-5,
         batch_size: int = 10,
         shuffle: bool = False,
         max_iter: int = 200,
@@ -121,6 +135,14 @@ class Classifier(Network):
             max_iter=max_iter,
         )
 
+    def fit(
+        self,
+        X: np.ndarray,
+        y: np.ndarray,
+        X_val: None | np.ndarray = None,
+        y_val: None | np.ndarray = None,
+    ):
+        # add output layer with one logistic unit
         output = Layer(
             units=1,
             activation="logistic",
@@ -130,8 +152,10 @@ class Classifier(Network):
         )
         self.layers.append(output)
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        super().fit(X, y)
+        if y_val is not None:
+            super().fit(X, y.reshape(-1, 1), X_val, y_val.reshape(-1, 1))
+        else:
+            super().fit(X, y.reshape(-1, 1), X_val, y_val)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         return np.round(self.forward(X)[:, 0])
@@ -140,12 +164,12 @@ class Classifier(Network):
 class Regressor(Network):
     def __init__(
         self,
-        hidden_layer_sizes=(5,),
+        hidden_layer_sizes=(100,),
         activation: str = "logistic",
-        learning_rate: float = 0.1,
+        learning_rate: float = 0.01,
         lam: float = 0.0001,
         alpha: float = 0.5,
-        tol: float = 1e-4,
+        tol: float = 1e-5,
         batch_size: int = 10,
         shuffle: bool = False,
         max_iter: int = 200,
@@ -162,8 +186,19 @@ class Regressor(Network):
             max_iter=max_iter,
         )
 
+    def fit(
+        self,
+        X: np.ndarray,
+        y: np.ndarray,
+        X_val: None | np.ndarray = None,
+        y_val: None | np.ndarray = None,
+    ):
+        # see if output should be 1-Dimensional or N-Dimensional
+        n_outputs = 1 if (len(y.shape) == 1) else y.shape[1]
+
+        # add the output layer with
         output = Layer(
-            units=1,
+            units=n_outputs,
             activation="linear",
             learning_rate=self.learning_rate,
             lam=self.lam,
@@ -171,8 +206,7 @@ class Regressor(Network):
         )
         self.layers.append(output)
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        super().fit(X, y)
+        super().fit(X, y, X_val, y_val)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        return self.forward(X)[:, 0]
+        return self.forward(X)
