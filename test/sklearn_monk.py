b1d9b0b271c309eaad5299c2abd85b5168f02f29
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-12 19:35:39 +0100

    sklearn monk

--- test/sklearn_monk.py
+++ test/sklearn_monk.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import OneHotEncoder
 
-from neural.network import Classifier
-from neural.utils import save_stats, stats
-from neural.validation import grid_search
+from neural.utils import save_stats
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -31,52 +31,56 @@ if __name__ == "__main__":
 
     hyperparams = {
         "hidden_layer_sizes": [(3,)],
-        "activation": ["logistic", "tanh"],
-        "learning_rate": [0.001, 0.003, 0.01, 0.03, 0.1],
-        "lam": [0.0, 0.0001],
-        "alpha": [0.0, 0.7, 0.9],
-        "tol": [1e-6],
-        "batch_size": [8, 16, 32],
+        "activation": ["tanh"],
+        "learning_rate_init": [0.001, 0.003, 0.01],
+        "alpha": [0.0, 0.0001],
+        "momentum": [0.0, 0.7],
+        "batch_size": [8, 16],
         "shuffle": [False, True],
-        "max_iter": [2000],
     }
 
     # if --gs argument is passed the grid search is performed
     if args.gs:
-        net, score = grid_search(
-            model_type=Classifier,
-            hyperparams=hyperparams,
-            X=X_train,
-            y=y_train,
-            k=10,
-            score_metric=accuracy_score,
-            scale=False,
-            verbose=True,
+        net = MLPClassifier(solver="sgd", tol=1e-5, max_iter=2000)
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+        grid = GridSearchCV(
+            net,
+            hyperparams,
+            scoring="accuracy",
+            n_jobs=-1,
+            cv=kf,
+            verbose=2,
         )
+
+        grid.fit(X_train, y_train)
+
+        net = grid.best_estimator_
+        score = grid.best_score_
+
     else:  # otherwise reads parameters from file and perform a single training
-        with open(f"results/monk{args.id}.json", "r") as fp:
+        with open(f"results/sklearn_monk{args.id}.json", "r") as fp:
             data = json.load(fp)
             params = data["parameters"]
 
-        net = Classifier(**params)
-        net.fit(X_train, y_train, X_test, y_test)
+        net = MLPClassifier(**params)
+        net.fit(X_train, y_train)
 
     # training accuracy
     net_pred = net.predict(X_train)
     train_score = accuracy_score(y_train, net_pred)
+    print(f"train score: {train_score:.2f}")
 
     # test accuracy
     net_pred = net.predict(X_test)
     test_score = accuracy_score(y_test, net_pred)
+    print(f"test score: {test_score:.2f}")
 
     # if --gs passed save results of grid search to a file
-    stats(net, hyperparams, train_score, test_score)
     if args.gs:
-        save_stats(net, hyperparams, score, f"results/monk{args.id}.json")
+        save_stats(net, hyperparams, score, f"results/sklearn_monk{args.id}.json")
 
     plt.title("Loss Curve")
-    plt.plot(net.loss_curve, label="training")
-    plt.plot(net.val_loss_curve, label="test")
+    plt.plot(net.loss_curve_, label="training")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.legend()
