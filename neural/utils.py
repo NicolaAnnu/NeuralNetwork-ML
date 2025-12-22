fb3edeb10d00d388d4307e0e028f7b9101c0e5a4
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-22 18:36:20 +0100

    dump and load results

--- neural/utils.py
+++ neural/utils.py
@@ -1,5 +1,21 @@
-import pandas as pd
+import json
+from pathlib import Path
 
 
-def save_results(filepath, df):
-    pass
+def dump_results(filepath: str, results: list[dict]):
+    path = Path(filepath)
+    if not path.exists():
+        with open(path, "w+") as fp:
+            json.dump(results, fp, indent=2)
+    else:
+        with open(path, "r+") as fp:
+            data = json.load(fp)
+            data.extend(results)
+            fp.seek(0)
+            json.dump(data, fp, indent=2)
+            fp.truncate()
+
+
+def load_results(filepath: str) -> list[dict]:
+    with open(filepath, "r") as fp:
+        return json.load(fp)
