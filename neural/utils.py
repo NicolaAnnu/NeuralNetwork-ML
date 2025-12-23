5b51e4cc5e68e7dbdb4451bbcfdaacb782ddf699
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-23 02:49:24 +0100

    plot 4 targets

--- neural/utils.py
+++ neural/utils.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+
 
 def dump_results(filepath: str, results: list[dict]):
     path = Path(filepath)
@@ -19,3 +21,15 @@ def dump_results(filepath: str, results: list[dict]):
 def load_results(filepath: str) -> list[dict]:
     with open(filepath, "r") as fp:
         return json.load(fp)
+
+
+def target_plot(y_true, y_pred):
+    _, axes = plt.subplots(2, 2, figsize=(8, 5), dpi=150, constrained_layout=True)
+
+    for i, ax in enumerate(axes.flat):
+        ax.scatter(y_pred[:, i], y_true[:, i], alpha=0.6)
+        ax.set_xlabel("predicted", fontsize=10)
+        ax.set_ylabel("target", fontsize=10)
+        ax.set_title(f"output {i + 1}", fontsize=10)
+
+    plt.show()
